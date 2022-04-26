757614e8b6ef71cd38821e9d01788065a03ad4e0
Angelina <angelinarwhite@gmail.com>
2022-04-26 10:15:58 -0400

    commit tree 5e5bb780d405736e379b42e49ab7fb85ec6c07d9

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // code your solution here
+function saturdayFun(activity)
+{
+    switch(activity)
+    {
+        case "bathe my dog":
+            return "This Saturday, I want to bathe my dog!";
+        default:
+            return "This Saturday, I want to roller-skate!";
+    }
+}
+
+function mondayWork(activity)
+{
+    switch(activity)
+    {
+        case "work from home":
+            return "This Monday, I will work from home.";
+        default:
+            return "This Monday, I will go to the office.";
+    }
+}
+
+function wrapAdjective(special)
+{
+    let string1 = "You are ";
+    return function(string2)
+    {
+        return string1 + special + string2 + special + "!";
+    }
+}
